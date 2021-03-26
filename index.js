b0bca14f248cdf7083d0cd45fda7f0ed4c6fab4b
vincent <vincent.arteaga@gmail.com>
2021-03-26 09:42:43 -0700

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+function findMatching (names, str) {
+     return names.filter(x => x.toLowerCase() === str.toLowerCase() )
+}
+
+function fuzzyMatch (names, str) {
+    return names.filter(x => x.slice(0,str.length) === str)
+}
+function matchName(drivers, str) {
+    return drivers.filter(x => x.name === str)
+}
